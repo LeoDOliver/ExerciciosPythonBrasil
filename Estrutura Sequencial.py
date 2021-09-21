55bb7d0b6aed38847940da921f179f7b845e5e92
LeoDOliver <77425598+LeoDOliver@users.noreply.github.com>
2021-09-21 19:04:21 -0300

    Add files via upload

---
+++ Estrutura Sequencial.py
@@ -0,0 +1,66 @@
+#Faça um Programa que mostre a mensagem "Alo mundo" na tela.
+
+"""
+print("Alo Mundo")
+"""
+
+#Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+"""
+n = int(input("Digite um número: "))
+print("O número informado foi %d." %n )
+"""
+
+#Faça um Programa que peça dois números e imprima a soma.
+
+"""
+n1 = int(input("Digite um número: "))
+n2 = int(input("Digite outro número: "))
+soma = n1 + n2
+print("A soma dos números é igual a %i." %soma )
+"""
+
+#Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+"""
+n1 = float(input("Digite a nota do 1ºbimestre: "))
+n2 = float(input("Digite a nota do 2ºbimestre: "))
+n3 = float(input("Digite a nota do 3ºbimestre: "))
+n4 = float(input("Digite a nota do 4ºbimestre: "))
+media = (n1 + n2 + n3 + n4)/4
+print("A sua média bimestral foi %.1f" %media )
+"""
+
+#Faça um Programa que converta metros para centímetros.
+
+"""
+m = float(input("Digite o valor em metros: "))
+cm = m*100
+print("O valor em centímetros é igual a %.f cm" %cm)
+"""
+
+#Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.
+
+"""
+r = float(input("Digite o raio do círculo: "))
+A = 3.14 * (r**2)
+print("A área do círculo é igual a %.2f" %A)
+"""
+
+#Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+"""
+l = float(input("Digite o lado do quadrado: "))
+A = l**2
+print("O dobro da aŕea deste quadrado é igual a %.1f" %(2*A))
+"""
+
+#Faça um Programa que pergunte quanto você ganha por hora e o número de horas trabalhadas no mês.
+#Calcule e mostre o total do seu salário no referido mês.
+
+"""
+ganho = float(input("Quanto você ganha por hora? "))
+horas = float(input("Quantas horas você trabalhou esse mês? "))
+st = ganho * horas
+print("O seu salário total no mês foi R$ %.2f" %st)
+"""
