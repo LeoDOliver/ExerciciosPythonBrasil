cfeb9f2cee7a967cb90774047bddad85ba447ab9
LeoDOliver <77425598+LeoDOliver@users.noreply.github.com>
2026-07-14 22:55:36 -0300

    Update Estrutura de Decisão.py

--- Estrutura de Decisão.py
+++ Estrutura de Decisão.py
@@ -1,1 +1,11 @@
+#01 - Faça um programa que peça dois números e imprima o maior deles.
 
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+
+if num1 > num2:
+  print(num1, "é maior do que ", num2)
+elif num2 > num1:
+  print(num2, "é maior do que ", num1)
+else:
+  print("Os números digitados são iguais")
