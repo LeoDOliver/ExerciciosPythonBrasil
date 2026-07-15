7780e33c9e6056b99c324bcbb7f4adc82af6c010
LeoDOliver <77425598+LeoDOliver@users.noreply.github.com>
2026-07-15 20:08:35 -0300

    Update Estrutura de Decisão.py

--- Estrutura de Decisão.py
+++ Estrutura de Decisão.py
@@ -9,3 +9,17 @@ elif num2 > num1:
   print(num2, "é maior do que ", num1)
 else:
   print("Os números digitados são iguais")
+
+
+
+
+#02 - Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo.
+
+num = int(input("Digite o número: "))
+
+if num < 0:
+    print("Número negativo")
+elif num > 0:
+    print("Número positivo")
+else:
+    print("O número digitado é 0, ou seja, nulo.")
