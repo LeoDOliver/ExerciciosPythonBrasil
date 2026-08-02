39265d94f1508489bac33bd7ad2b4af0a93befb5
LeoDOliver <77425598+LeoDOliver@users.noreply.github.com>
2026-08-02 10:18:35 -0300

    Update Estrutura de Decisão.py

--- Estrutura de Decisão.py
+++ Estrutura de Decisão.py
@@ -73,3 +73,19 @@ elif media < 7 and media > 0:
 else:
   print("As notas informadas geraram um valor invalido.")
 
+
+
+#Exercício 06 - Faça um programa que leia três números e mostre o maior deles:
+
+num1 = int(input("Digite o primeiro número: "))
+num2 = int(input("Digite o segundo número: "))
+num3 = int(input("Digite o terceiro número: "))
+
+if num1 > num2 and num1 > num3:
+  print(f"{num1} é o maior.")
+elif num2 > num1 and num2 > num3:
+  print(f"{num2} é o maior.")
+elif num3 > num1 and num3 > num2:
+  print(f"{num3} é o maior.")
+else:
+  print("Os números informados são iguais.")
