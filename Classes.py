724d1bd1d3483b4c626468ffc5b2d47e2053d5a8
LeoDOliver <77425598+LeoDOliver@users.noreply.github.com>
2026-08-07 17:49:40 -0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -1,1 +1,27 @@
+"""Exercício 01 - Crie uma classe que modele uma bola:
 
+    Atributos: cor, circunferência, material
+    Métodos: troca_cor e mostra_cor
+"""
+
+class Bola:
+    def __init__(self, cor, circunferencia, material):
+        self.cor = cor
+        self.circunferencia = circunferencia
+        self.material = material
+
+    def troca_cor(self, nova_cor):
+        self.cor = nova_cor
+        print("Cor alterada.")
+
+    def mostra_cor(self):
+        print(f"A cor da bola é {self.cor}")
+
+
+Bola1 = Bola("verde", 3.0, "Borracha")
+
+Bola1.mostra_cor()
+
+nova_cor = input("Digite a nova cor: ")
+Bola1.troca_cor(nova_cor)
+Bola1.mostra_cor()
