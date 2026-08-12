0c8bd56e3b4bb68864bec41cabc72997e43bf74f
LeoDOliver <77425598+LeoDOliver@users.noreply.github.com>
2026-08-12 17:49:52 -0300

    Update Estrutura de Repetição.py

--- Estrutura de Repetição.py
+++ Estrutura de Repetição.py
@@ -1,1 +1,14 @@
+"""Exercício 1
 
+Faça um programa que peça uma nota, entre zero e dez. Mostre uma mensagem caso o valor seja inválido e continue pedindo até que o usuário informe um valor válido.
+"""
+
+nota = int(input("Digite um nota entre 0 e 10: "))
+while True:
+    if nota > 10 or nota < 0:
+        nota = int(input("Digite um nota entre 0 e 10: "))
+    else:
+        break
+
+print(f"Nota aceita: {nota}")
+  
