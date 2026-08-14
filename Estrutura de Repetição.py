502857fa79c1b3e50560186da577519524ba7453
LeoDOliver <77425598+LeoDOliver@users.noreply.github.com>
2026-08-14 16:02:15 -0300

    Update Estrutura de Repetição.py

--- Estrutura de Repetição.py
+++ Estrutura de Repetição.py
@@ -12,3 +12,17 @@ while True:
 
 print(f"Nota aceita: {nota}")
   
+
+"""Exercício 2 - Faça um programa que leia um nome de usuário e a sua senha e não aceite a senha igual ao nome do usuário, mostrando uma mensagem de erro e voltando a pedir as informações.
+"""
+
+username = input("Digite o nome de usuário: ")
+senha =  input("Digite a senha: ")
+
+while True:
+    if username == senha:
+        print("Nome de usuário e senha não podem ser iguais!")
+        username = input("Digite o nome de usuário: ")
+        senha =  input("Digite a senha: ")
+    else:
+        break
