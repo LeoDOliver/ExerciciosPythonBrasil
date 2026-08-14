b16a9fd7acf6d4b595833ae5874d8e58e8cdbbff
LeoDOliver <77425598+LeoDOliver@users.noreply.github.com>
2026-08-14 16:10:57 -0300

    Update Classes.py

--- Classes.py
+++ Classes.py
@@ -99,3 +99,8 @@ n_conta = input("Digite o número da sua conta: ")
 saldo = float(input("Digite o saldo da conta: "))
 
 Conta = ContaBancaria(n_conta, nome, saldo)
+
+"""Exercício 06 - Faça um programa que simule um televisor criando-o como um objeto. 
+O usuário deve ser capaz de informar o número do canal e aumentar ou diminuir o volume. Certifique-se de que o número do canal e o nível do volume permanecem dentro de faixas válidas."""
+
+
